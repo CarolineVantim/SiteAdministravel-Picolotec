b582651f0a59c6ef3a67bcbe646dc38619d8a484
carolinevantim <caroline.vantim@picolotec.com.br>
2022-08-30 15:53:13 -0300

    adicionando novas rotas de acesso no painel administrativo

---
+++ templates/admin/pages/create.tpl.php
@@ -0,0 +1,5 @@
+<h3>Administração de cadastros</h3>
+
+//Criação de páginas
+
+<a href="/admin">Ir para home</a>
---
+++ templates/admin/pages/index.tpl.php
@@ -0,0 +1,5 @@
+<h3>Administração de cadastros</h3>
+
+//Listagem
+
+<a href="/admin">Ir para home</a>
