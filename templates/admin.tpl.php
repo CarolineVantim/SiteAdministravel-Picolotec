a4b660b5e073cb9d6d46653fb21c36db4a22cca2
carolinevantim <caroline.vantim@picolotec.com.br>
2022-08-30 15:07:39 -0300

    adicionando estilização da página admin

--- templates/admin.tpl.php
+++ templates/admin.tpl.php
@@ -1,13 +1,44 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Painel administrativo</title>
-</head>
-<body>
-    <h1>Administração</h1>
-    <?php include $content ?>
-</body>
+    <link href="/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/style.css">
+  </head>
+  <body class="d-flex flex-column">
+    <div id="header">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a href="" class="navbar-brand">Administrativo</a>
+            <spn class="navbar-text">
+                Painel administrativo!
+            </spn>
+        </nav>
+    </div>
+    <div id="main">
+        <div class="row">
+            <div class="col">
+                <ul id="main-menu" class="nav flex-column nav-pills bg-secondary text-white p-2">
+                    <li class="nav-item">
+                        <span class="nav-link text-white-40"><small>Menu</small>
+                        </span>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link active">Páginas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="" class="nav-link">Usuários</a>
+                    </li>
+                </ul>
+            </div>
+            <div id="content" class="col-10">
+                <?php include $content ?>
+            </div>
+        </div>
+    
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
 </html>
