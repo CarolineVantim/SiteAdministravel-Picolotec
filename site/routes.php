865ea5d2ae3bfb8d49e413c8986ed40c383ad434
carolinevantim <caroline.vantim@picolotec.com.br>
2022-09-06 14:55:13 -0300

    formulário site finalizado

--- site/routes.php
+++ site/routes.php
@@ -3,6 +3,21 @@
 require __DIR__ . '/../admin/pages/db.php';
 
 if (resolve('/contato')){
+    if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+        $from = filter_input(INPUT_POST, 'from');
+        $subject = filter_input(INPUT_POST, 'subject');
+        $message = filter_input(INPUT_POST, 'message');
+        $headers = 'FROM: ' . $from . "\r\n" .
+            'Reply-To: ' . $from . "\r\n" . 
+            'X-Mailer: PHP/' . phpversion();
+
+        if (mail('cvantim@gmail.com', $subject, $message, $headers)){
+            flash('Email enviado com sucesso');
+        }else{
+            flash('Seu e-mail não foi enviado!');
+        }
+        return header('Location: /contato');
+    }
     $pages = $pages_all();
     render('site/contato', 'site', compact('pages'));
 }elseif ($params = resolve('/(.*)')){
